4ee287c3055e7f73fbb49e30477e043a1fccce71
Sadiq Salau <sadiqsalau888@gmail.com>
2025-10-20 14:29:06 +0100

    feat: Removed App.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useRef, useState } from "react";
-import "./App.css";
 import { Controller, useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
