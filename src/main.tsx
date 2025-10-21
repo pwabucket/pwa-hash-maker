692be2b99d31a46594e0ae83c973ec559faed51d
Sadiq Salau <sadiqsalau888@gmail.com>
2025-10-21 08:56:59 +0100

    feat: Registered Service Worker

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
+import { registerSW } from "virtual:pwa-register";
+
+/** Register Service Worker */
+registerSW({ immediate: true });
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
