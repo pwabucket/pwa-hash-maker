9097882cde99f468603e8df7646cb221561c389e
Sadiq Salau <sadiqsalau888@gmail.com>
2025-10-21 08:32:14 +0100

    feat: Display as a flex with centered content

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -6,6 +6,7 @@ const Button = (props: React.ComponentProps<"button">) => {
       {...props}
       className={cn(
         "bg-blue-500 text-white rounded-xl p-2 cursor-pointer",
+        "flex justify-center items-center gap-2",
         "disabled:opacity-50 disabled:cursor-not-allowed",
         props.className
       )}
