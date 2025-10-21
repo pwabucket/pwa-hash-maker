40ae1d44c0d3e4d0d0d9fe4972909f902d9e7dbf
Sadiq Salau <sadiqsalau888@gmail.com>
2025-10-21 08:50:08 +0100

    feat: Configured PWA Plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,65 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import process from "process";
+import version from "vite-plugin-package-version";
+import { ViteEjsPlugin } from "vite-plugin-ejs";
+import { VitePWA } from "vite-plugin-pwa";
+
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    tailwindcss(),
-    react({
-      babel: {
-        plugins: [["babel-plugin-react-compiler"]],
-      },
-    }),
-  ],
+export default defineConfig(({ mode }) => {
+  /** Env */
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [
+      /** Plugins */
+      version(),
+      VitePWA({
+        registerType: "autoUpdate",
+        workbox: {
+          globPatterns: ["**/*.*"],
+          maximumFileSizeToCacheInBytes: 5 * 1024 ** 2,
+        },
+        manifest: {
+          name: "Hash Maker",
+          short_name: "Hash Maker",
+          description:
+            "Transfer USDT on the Binance Smart Chain with desired transaction hash suffix.",
+          theme_color: "#27272a",
+          background_color: "#27272a",
+          icons: [
+            {
+              src: "pwa-64x64.png",
+              sizes: "64x64",
+              type: "image/png",
+            },
+            {
+              src: "pwa-192x192.png",
+              sizes: "192x192",
+              type: "image/png",
+            },
+            {
+              src: "pwa-512x512.png",
+              sizes: "512x512",
+              type: "image/png",
+            },
+            {
+              src: "maskable-icon-512x512.png",
+              sizes: "512x512",
+              type: "image/png",
+              purpose: "maskable",
+            },
+          ],
+        },
+      }),
+      ViteEjsPlugin(env),
+      tailwindcss(),
+      react({
+        babel: {
+          plugins: [["babel-plugin-react-compiler"]],
+        },
+      }),
+    ],
+  };
 });
